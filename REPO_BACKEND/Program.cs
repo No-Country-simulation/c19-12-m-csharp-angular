1a420be45ae6a74c357d2ae2ce0168c915d69d19
Matiu <mateovranych@hotmail.com>
2024-07-05 12:04:13 -0300

    Conexión a la base de datos e instalación de identity

--- REPO_BACKEND/Program.cs
+++ REPO_BACKEND/Program.cs
@@ -1,6 +1,17 @@
+using backnc.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//Hago una cadena de conexión que va a servir para enlazarse con el appsettings.json.
+string connectionString = builder.Configuration.GetConnectionString("defaultConnection");
+//Hago una variable llamada ServerVersion.
+var ServerVersion = new MySqlServerVersion(new Version(8, 0, 33));
+//En la siguiente linea hago la conexión.
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString , ServerVersion));
+
+
 
 // Add services to the container.
 
