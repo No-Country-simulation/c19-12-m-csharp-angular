708c570599606e2b0062aeb95e376e64175729d5
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-07-08 13:24:37 -0300

    index page

--- REPO_FRONTEND/src/app/shared/components/header/header.component.ts
+++ REPO_FRONTEND/src/app/shared/components/header/header.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../../../material/material.module';
 
-
-
 @Component({
   selector: 'shared-header',
   standalone: true,
