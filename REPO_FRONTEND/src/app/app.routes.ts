5c642e9be556ed20bea69b529305d2399c9f3f43
Matiu <mateovranych@hotmail.com>
2024-07-08 11:31:30 -0300

    maqueta completa

--- REPO_FRONTEND/src/app/app.routes.ts
+++ REPO_FRONTEND/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './core/components/index/index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'', component: IndexComponent
+}];
