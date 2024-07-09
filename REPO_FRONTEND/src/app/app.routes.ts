3f22faafedcbd8f321bfab0f2527b57898d1d7d2
Andres Diez <andresdieztuberquia@gmail.com>
2024-07-09 12:53:38 -0500

    Creacion del Login para el Frontend

--- REPO_FRONTEND/src/app/app.routes.ts
+++ REPO_FRONTEND/src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './core/components/index/index.component';
+import { LoginComponent } from './auth/components/login/login.component';
 
-export const routes: Routes = [{
-    path:'', component: IndexComponent
-}];
+export const routes: Routes = [
+    {
+        path: '',
+        component: IndexComponent,
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+    }
+];
--- REPO_FRONTEND/src/app/auth/components/login/login.component.ts
+++ REPO_FRONTEND/src/app/auth/components/login/login.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { MaterialModule } from '../../../material/material.module';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FooterComponent,
+    HeaderComponent,
+    MaterialModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
