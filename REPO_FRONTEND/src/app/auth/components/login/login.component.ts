8d47c9cc7eeebda5be6f56fc2e8f2d588d2694ec
jfpanchi <jfpanchi2@espe.edu.ec>
2024-07-10 09:09:55 -0500

    fix: :truck: Update routing
    Add index routes
    Add auth routes
    Add login page
    Add register page
    Add index page

--- REPO_FRONTEND/src/app/auth/components/login/login.component.ts
+++ REPO_FRONTEND/src/app/auth/components/login/login.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../../../shared/components/footer/footer.component';
-import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { MaterialModule } from '../../../material/material.module';
 
 @Component({
-  selector: 'app-login',
+  selector: 'auth-login',
   standalone: true,
   imports: [
-    FooterComponent,
-    HeaderComponent,
     MaterialModule
   ],
   templateUrl: './login.component.html',
--- REPO_FRONTEND/src/app/shared/components/header/header.component.ts
+++ REPO_FRONTEND/src/app/shared/components/header/header.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../../../material/material.module';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'shared-header',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  public isAuthRoute: boolean = false;
+
+  constructor(private router : Router){
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isAuthRoute = event.urlAfterRedirects.includes('auth');
+      }
+    });
+  }
+  
+  navigateTo(route : string){
+    this.router.navigate([route]);
+  }
+}
