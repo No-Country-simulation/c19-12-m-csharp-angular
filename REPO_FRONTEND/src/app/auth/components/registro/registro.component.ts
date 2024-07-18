b2d3310a55c48604d0bdfe7596f05ca28415c6a8
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-07-18 15:00:47 -0300

    register component

--- REPO_FRONTEND/src/app/auth/components/registro/registro.component.ts
+++ REPO_FRONTEND/src/app/auth/components/registro/registro.component.ts
@@ -1,14 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../../material/material.module';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'auth-registro',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, ReactiveFormsModule, CommonModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.scss',
 })
 export class RegistroComponent {
-  userName = new FormControl('');
+  private fb = inject(FormBuilder);
+
+  public formRegister = this.fb.group({
+    userName: ['', [Validators.required, Validators.minLength(6)]],
+    firstName: [''],
+    lastName: [''],
+    email: [''],
+    dni: [''],
+    phoneNumber: [''],
+    password: [''],
+    address: [''],
+    // countryId : [''],
+    // provinceId: [''],
+    // neighborhoodId : [''],
+  });
+
+  submit() {
+    console.log(this.formRegister.value);
+  }
 }
