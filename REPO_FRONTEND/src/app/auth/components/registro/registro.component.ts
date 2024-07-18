5554b534407ece73b438f4eb50332ae046be7a6d
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-07-18 11:24:30 -0300

    register html

--- REPO_FRONTEND/src/app/auth/components/registro/registro.component.ts
+++ REPO_FRONTEND/src/app/auth/components/registro/registro.component.ts
@@ -1,12 +1,14 @@
+import { MaterialModule } from '../../../material/material.module';
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'auth-registro',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './registro.component.html',
-  styleUrl: './registro.component.scss'
+  styleUrl: './registro.component.scss',
 })
 export class RegistroComponent {
-
+  userName = new FormControl('');
 }
