ec4dfb7a956e74e76e841254203ccc32a7b4ce0e
Matiu <mateovranych@hotmail.com>
2024-07-22 20:52:09 -0300

    Jobs,Profile

---
+++ REPO_BACKEND/Data/POCOEntities/Job.cs
@@ -0,0 +1,15 @@
+﻿namespace backnc.Data.POCOEntities
+{
+	public class Job
+	{
+		public int Id { get; set; }
+		public int UserId { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string ImageUrl { get; set; }
+		public DateTime CreatedAt { get; set; }
+
+		// Relación muchos a uno con Usuario
+		public User User { get; set; }
+	}
+}
