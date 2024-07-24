73a6b674c7ac9cc7f5e6fe494b0ea6b6abe0e8cc
Matiu <mateovranych@hotmail.com>
2024-07-24 05:12:36 -0300

    Profiles funcionando, faltan las validaciones que se van a conocer a un futuro

--- REPO_BACKEND/Service/ProfileService.cs
+++ REPO_BACKEND/Service/ProfileService.cs
@@ -1,34 +1,71 @@
 ﻿using backnc.Data.Interface;
 using backnc.Data.POCOEntities;
+using backnc.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace backnc.Service
 {
-	public class ProfileService
+	public class ProfileService 
 	{
 		private readonly IAppDbContext context;
-        public ProfileService(IAppDbContext context)
+		private readonly string rutaServidor;
+		private readonly string rutaAlmacenamiento;
+		public ProfileService(IAppDbContext context, IConfiguration configuration)
         {
             this.context = context;
-        }
-		public async Task<Profile> GetProfileByUser(int userId)
+			this.rutaAlmacenamiento = configuration["rutaImagenes"]!;
+			this.rutaServidor = configuration["rutaServidor"]!;
+		}
+		public async Task<List<Profile>> GetAllProfiles()
 		{
-			return await context.Profiles
-				.FirstOrDefaultAsync(p => p.UserId == userId);
+			return await context.Profiles.ToListAsync();
 		}
 
-		public async Task<Profile> CreateProfile(Profile profile)
+		public async Task<Profile> GetProfileByUser(int userId)
 		{
-			context.Profiles.Add(profile);
-			await context.SaveChangesAsync();
-			return profile;
+			return await context.Profiles.FirstOrDefaultAsync(p => p.UserId == userId);
 		}
+
+		//public async Task<Profile> CreateProfile(Profile profile)
+		//{
+		//	context.Profiles.Add(profile);
+		//	await context.SaveChangesAsync();
+		//	return profile;
+		//}
 
 		public async Task<Profile> UpdateProfile(Profile profile)
 		{
-			context.Profiles.Update(profile);
-			await context.SaveChangesAsync();
-			return profile;
+			var existingProfile = await context.Profiles.FirstOrDefaultAsync(p => p.Id == profile.Id);
+
+			if (existingProfile != null)
+			{
+				existingProfile.Specialty = profile.Specialty;
+				existingProfile.Experience = profile.Experience;
+				existingProfile.Description = profile.Description;
+				existingProfile.ImageUrl = profile.ImageUrl;
+				await context.SaveChangesAsync();
+			}
+
+			return existingProfile;
+		}
+
+
+
+		public async Task<string> SaveImageAsync(IFormFile image)
+		{
+			// Generar un nombre de archivo único
+			string fileName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+			// Ruta completa para almacenar la imagen
+			string fullPath = Path.Combine(rutaAlmacenamiento, fileName);
+
+			// Guardar la imagen en el sistema de archivos
+			using (var stream = new FileStream(fullPath, FileMode.Create))
+			{
+				await image.CopyToAsync(stream);
+			}
+			// Devolver la URL relativa de la imagen
+			return $"{rutaServidor}/images/{fileName}";
+			
 		}
 
 	}
