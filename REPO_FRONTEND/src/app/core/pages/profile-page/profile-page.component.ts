675e90eab8a2d7aa5ca18225e1d2a702438d5b3c
jfpanchi <jfpanchi2@espe.edu.ec>
2024-07-30 23:49:28 -0500

    feat: :sparkles: Add jobs
    Add job create
    Add job delete
    Add list jobs for client
    Add jobs in profile

--- REPO_FRONTEND/src/app/core/pages/profile-page/profile-page.component.ts
+++ REPO_FRONTEND/src/app/core/pages/profile-page/profile-page.component.ts
@@ -6,22 +6,37 @@ import { CardService } from '../../services/card.service';
 
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { Card, IdCard } from '../../models/card.interface';
-import { CategoriesToStringPipe } from "../../pipes/categories-to-string.pipe";
+import { CategoriesToStringPipe } from '../../pipes/categories-to-string.pipe';
+import { Job } from '../../models/job.interface';
+import { JobService } from '../../services/job.service';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
+import { MatDialog } from '@angular/material/dialog';
+import { JobDialogComponent } from '../../components/job-dialog/job-dialog.component';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [CommonModule, MaterialModule, HeaderComponent, RouterModule, CategoriesToStringPipe],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    HeaderComponent,
+    RouterModule,
+    CategoriesToStringPipe,
+    FooterComponent,
+  ],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss',
 })
 export class ProfilePageComponent {
   public card: Card | null = null;
+  public jobs: Job[] = [];
 
   constructor(
     private cardService: CardService,
+    private jobService: JobService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
@@ -45,5 +60,20 @@ export class ProfilePageComponent {
         this.router.navigate(['/']);
       }
     );
+    const jobs = this.jobService.getJobsByUserId(id).subscribe(
+      (response) => {
+        this.jobs = response;
+      },
+      (error) => {
+        console.error('Error loading jobs:', error);
+      }
+    );
+  }
+
+  openDialog(job: any): void {
+    this.dialog.open(JobDialogComponent, {
+      width: '400px',
+      data: job,
+    });
   }
 }
--- REPO_FRONTEND/src/app/core/routes/dashboard.routes.ts
+++ REPO_FRONTEND/src/app/core/routes/dashboard.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardLayoutComponent } from '../layouts/dashboard-layout/dashboard-layout.component';
 import { DashboardPageComponent } from '../pages/dashboard-page/dashboard-page.component';
+import { JobsPageComponent } from '../pages/jobs-page/jobs-page.component';
 
 export const dashboardRoutes: Routes = [
   {
@@ -8,6 +9,7 @@ export const dashboardRoutes: Routes = [
     component: DashboardLayoutComponent,
     children: [
       { path: '', component: DashboardPageComponent },
+      { path: 'jobs', component: JobsPageComponent },
       {
         path: '**',
         redirectTo: '',
