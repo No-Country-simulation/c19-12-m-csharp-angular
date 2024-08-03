e2fd853e6ac93b558854dd19dd1ed81a8f623efa
Matiu <mateovranych@hotmail.com>
2024-08-02 21:09:37 -0300

    Crud de clientes

--- REPO_BACKEND/Program.cs
+++ REPO_BACKEND/Program.cs
@@ -36,6 +36,7 @@ builder.Services.AddScoped<INeighborhoodService, NeighborhoodService>();
 builder.Services.AddScoped<IUserValidationService, UserValidationService>();
 builder.Services.AddScoped<ProfileService, ProfileService>();
 builder.Services.AddScoped<JobService, JobService>();
+builder.Services.AddScoped<ClienteService, ClienteService>();
 builder.Services.AddScoped<DataSeeder>();
 
 
