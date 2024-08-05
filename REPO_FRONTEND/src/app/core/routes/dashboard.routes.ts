20c84bb186ee9c1592a058fc0a948a65645eae61
jfpanchi <jfpanchi2@espe.edu.ec>
2024-08-05 17:44:38 -0500

    feat: :sparkles: Add actions of admins
    Add actions of admins

--- REPO_FRONTEND/src/app/core/routes/dashboard.routes.ts
+++ REPO_FRONTEND/src/app/core/routes/dashboard.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { DashboardLayoutComponent } from '../layouts/dashboard-layout/dashboard-layout.component';
 import { DashboardPageComponent } from '../pages/dashboard-page/dashboard-page.component';
 import { JobsPageComponent } from '../pages/jobs-page/jobs-page.component';
+import { AdminsPageComponent } from '../pages/admins-page/admins-page.component';
 
 export const dashboardRoutes: Routes = [
   {
@@ -10,6 +11,7 @@ export const dashboardRoutes: Routes = [
     children: [
       { path: '', component: DashboardPageComponent },
       { path: 'jobs', component: JobsPageComponent },
+      { path: 'admins', component: AdminsPageComponent },
       {
         path: '**',
         redirectTo: '',
