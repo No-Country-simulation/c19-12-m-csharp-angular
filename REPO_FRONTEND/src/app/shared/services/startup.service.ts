5a1f0edd8655f6d065c95ec25cfc4034a1110ceb
jfpanchi <jefpanchi@gmail.com>
2024-11-06 15:35:58 -0500

    fix: :green_heart:  Add connection
    Add connection with https

--- REPO_FRONTEND/src/app/shared/services/startup.service.ts
+++ REPO_FRONTEND/src/app/shared/services/startup.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class StartupService {
-  private url = 'http://www.mercado-chamba.somee.com';
+  private url = 'https://www.mercado-chamba.somee.com';
 
   constructor(private http: HttpClient) { }
 
