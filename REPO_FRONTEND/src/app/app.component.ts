a813c4c7535a041be7d086cdd6ec9ace5f1da5e1
jfpanchi <jefpanchi@gmail.com>
2024-11-06 22:13:28 -0500

    fix: :green_heart:  Add connection
    Remove initial connection

--- REPO_FRONTEND/src/app/app.component.ts
+++ REPO_FRONTEND/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { StartupService } from './shared/services/startup.service';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +10,4 @@ import { StartupService } from './shared/services/startup.service';
 })
 export class AppComponent implements OnInit {
   title = 'chicos';
-
-  constructor(private startupService: StartupService) {}
-
-  ngOnInit(): void {
-    this.startupService.callStartupEndpoint().subscribe(
-      (response) => {
-        console.log('Conexion habilitada:', response);
-      },
-      (error) => {
-        console.error('Error al habilitar la conexion:', error);
-      }
-    );
-  }
 }
